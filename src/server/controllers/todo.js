0f7712032d30f25247f426a194579491b403abb6
James Mundie <jamesdrmundie@gmail.com>
2020-01-06 11:04:57 +0000

    fixed express initial run

--- src/server/controllers/todo.js
+++ src/server/controllers/todo.js
@@ -1,8 +1,21 @@
+const state = {todos: [
+    {id:Math.random().toString(36).substring(7), value: "buy shopping", completed: false},
+    {id:Math.random().toString(36).substring(7), value: "second", completed: false},
+]}
+
 function todo (req, res) {
     res.json({todos: [
         {id:Math.random().toString(36).substring(7), value: "buy shopping", completed: false},
         {id:Math.random().toString(36).substring(7), value: "second", completed: false},
     ]})
+}
+
+function getTodos(req, res) {
+    res.json(state)
+}
+
+function addTodo(req, res) {
+    req.body
 }
 
 export default todo;
--- src/server/index.js
+++ src/server/index.js
@@ -1,11 +1,22 @@
 require( '@babel/register')
+const express = require('express')
+const bodyParser = require('body-parser')
 
 const controllers = require('./controllers/index')
 
-const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', controllers)
+const jsonParser = bodyParser.json()
+
+console.log(controllers.default)
+app.get('/', controllers.default)
+
+const apiRouter = new express.Router();
+app.get('/todos', controllers.default)
+app.post('/todo', jsonParser, controllers.default)
+
+app.use('/api/todo-list', apiRouter)
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
